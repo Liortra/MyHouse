9266db6cb05bc3643d103ded177f90adf5320406
Lior Trachtman <39022937+Liortra@users.noreply.github.com>
2020-08-16 21:59:21 +0300

    Update Doors

---
+++ Assets/Scripts/SlideMotion.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlideMotion : MonoBehaviour
+{
+    public bool isOpen;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isOpen = false;
+        animator = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "MainCamera")
+        {
+            isOpen = true;
+            animator.SetTrigger("OpenSlide");
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (isOpen)
+        {
+            isOpen = false;
+            animator.SetTrigger("CloseSlide");
+        }
+    }
+}
